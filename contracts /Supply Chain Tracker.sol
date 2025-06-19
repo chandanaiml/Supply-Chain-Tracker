f7a2d8c0e066fcf5f2b0baab850f664d4d124207
chandanaiml <0126AL231049@oriental.ac.in>
2025-06-19 07:11:45 -0700

    Update Supply Chain Tracker.sol

--- contracts /Supply Chain Tracker.sol
+++ contracts /Supply Chain Tracker.sol
@@ -1,1 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SupplyChainTracker {
+    enum Status { Manufactured, InTransit, Delivered }
+
+    struct Product {
+        uint256 id;
+        string name;
+        address owner;
+        Status status;
+    }
+
+    mapping(uint256 => Product) public products;
+    uint256 public productCount;
+
+    event ProductAdded(uint256 id, string name, address indexed owner);
+    event ProductStatusUpdated(uint256 id, Status newStatus);
+
+    function addProduct(string memory _name) public {
+        productCount++;
+        products[productCount] = Product(productCount, _name, msg.sender, Status.Manufactured);
+        emit ProductAdded(productCount, _name, msg.sender);
+    }
+
+    function updateStatus(uint256 _id, Status _status) public {
+        require(products[_id].owner == msg.sender, "Not the product owner");
+        products[_id].status = _status;
+        emit ProductStatusUpdated(_id, _status);
+    }
+
+    function getProduct(uint256 _id) public view returns (Product memory) {
+        return products[_id];
+    }
+}
 
